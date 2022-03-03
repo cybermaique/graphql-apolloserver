a44d408b05c557a5c1b547ea7d94ab5012b10eda
cybermaique <maiqued.18@gmail.com>
2022-03-02 22:34:43 -0300

    form user

--- frontend/src/components/NewUserForm.tsx
+++ frontend/src/components/NewUserForm.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 export function NewUserForm() {
-    fucntion handleCreateUser() {}
+    const [name, setName] = useState('');
+
+    function handleCreateUser() {
+        
+    }
 
     return (
         <form onSubmit={handleCreateUser}>
-
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            
         </form>
     )
 }
