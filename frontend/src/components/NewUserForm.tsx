0d80b5656219ffa322d690eaa2b9197aebac49c6
cybermaique <maiqued.18@gmail.com>
2022-03-04 22:38:54 -0300

    form user

--- frontend/src/components/NewUserForm.tsx
+++ frontend/src/components/NewUserForm.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { gql } from "@apollo/client";
+import { FormEvent, useState } from "react";
+
+const CREATE_USER = gql`
+
+`;
 
 export function NewUserForm() {
     const [name, setName] = useState('');
 
-    function handleCreateUser() {
-        
+    function handleCreateUser(event: FormEvent) { //recebe evento do formevent que é de dentro do react 
+        event.preventDefault(); //nao atualizar pagina quando der submit no formulario (que é o comportamento padrao dos formularios no html)
     }
 
     return (
