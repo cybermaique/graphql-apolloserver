3d13cf1c9f335c334b0386e4cf2f84052a664e90
cybermaique <maiqued.18@gmail.com>
2022-03-05 22:42:25 -0300

    form user

--- frontend/src/components/NewUserForm.tsx
+++ frontend/src/components/NewUserForm.tsx
@@ -1,21 +1,35 @@
-import { gql } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import { FormEvent, useState } from "react";
 
 const CREATE_USER = gql`
-
+    mutation($name: string){
+        createUser(name: $name) {
+            id
+            name
+        }
+    }
 `;
 
 export function NewUserForm() {
     const [name, setName] = useState('');
+    const [createUser, { data, loading, error }] = useMutation(CREATE_USER)
 
-    function handleCreateUser(event: FormEvent) { //recebe evento do formevent que é de dentro do react 
+    async function handleCreateUser(event: FormEvent) { //recebe evento do formevent que é de dentro do react 
         event.preventDefault(); //nao atualizar pagina quando der submit no formulario (que é o comportamento padrao dos formularios no html)
+    
+
+        if (!name) {
+            return;
+        }
+
+        await createUser
     }
+
 
     return (
         <form onSubmit={handleCreateUser}>
             <input type="text" value={name} onChange={e => setName(e.target.value)} />
             
         </form>
-    )
+    );
 }
